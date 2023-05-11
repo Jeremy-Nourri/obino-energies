9b8e6441eb3d2a37229baab62d44a600f36fbdde
Jeremy Nourri <108303634+Jeremy-Nourri@users.noreply.github.com>
2023-05-11 13:01:48 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Obino energies`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
